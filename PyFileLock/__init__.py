8c66e8fb31d5c56866df28f1565063ba956b2e98
aylak-github <yk3514003@gmail.com>
2022-02-21 03:16:55 +0300

    🔐 PyFileLock - 1.0.0

---
+++ PyFileLock/__init__.py
@@ -0,0 +1,8 @@
+# @G4rip - < https://t.me/G4rip >
+# Copyright (C) 2022
+# Tüm hakları saklıdır.
+#
+# Bu dosya, < https://github.com/aylak-github/PyFileLock > parçasıdır.
+# Lütfen GNU Affero Genel Kamu Lisansını okuyun;
+# < https://www.github.com/aylak-github/PyFileLock/blob/master/LICENSE/ >
+# ================================================================
